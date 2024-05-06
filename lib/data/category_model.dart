0af6ed04cfed402f07abd9ab989ea8b13f636f0c
SELMA_SUGEY_MENDOZA_CASTREJON <a.21308051280386@cbtis128.edu.mx>
2024-05-06 22:49:17 +0000

    Actualizado

---
+++ lib/data/category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  final int id;
+  final String name;
+
+  Category(this.id, this.name);
+}
+
+List<Category> categories = [
+  Category(0, 'Todo'),
+  Category(1, 'Artistas'),
+  Category(2, 'Canciones'),
+  Category(3, 'Playlist'),
+  Category(4, 'Albums'),
+];
---
+++ lib/data/plant_data.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:ui_13/data/plant_model.dart';
+
+List<Cantantes> plants = [
+  Cantantes(
+    id: 0,
+    name: 'NewJeans',
+    imagePath: 'assets/images/artista1.jpg',
+    category: '16,546,789',
+    description:
+        'NewJeans (MINJI, HANNI, DANIELLE, HAERIN y HYEIN) debutaron el 22 de julio de 2022 como el primer grupo de ADOR, un sello independiente bajo HYBE liderado por Hee Jin Min. El quinteto aspira a superar los límites del K-pop con música honesta y auténtica que rezuma juventud.',
+    price: 474,
+    isFavorit: false,
+  ),
+  Cantantes(
+    id: 1,
+    name: 'Sabrina Carpenter',
+    imagePath: 'assets/images/artista2.jpg',
+    category: '50,213,879',
+    description:
+        'Sabrina Carpenter ha cautivado a una audiencia de millones como cantante, compositora, actriz e ícono de estilo. Con su música ha cantado un himno tras otro en el escenario y en el estudio.',
+    price: 50,
+    isFavorit: false,
+  ),
+  Cantantes(
+    id: 2,
+    name: 'Dua lipa',
+    imagePath: 'assets/images/artista3.jpg',
+    category: '77,984,132',
+    description:
+        'Inspirado en el propio autodescubrimiento de Dua, Radical Optimism (que se lanzará el 3 de mayo) es el tercer álbum de Dua Lipa, la potencia pop ganadora de 3 premios GRAMMY y 7 premios BRIT.',
+    price: 7,
+    isFavorit: false,
+  ),
+  Cantantes(
+    id: 3,
+    name: 'Olivia Rodrigo',
+    imagePath: 'assets/images/artista4.jpg',
+    category: '56,369,258',
+    description:
+        'Después de batir récords con su álbum debut 4x Platino, SOUR, que encabezó las listas, el álbum más rápido de la historia en tener todas sus canciones certificadas RIAA Platino o superior.',
+    price: 33,
+    isFavorit: false,
+  ),
+];
+
+List<Cantantes> populerPlants = [
+  Cantantes(
+    id: 0,
+    name: 'Ariana Grande',
+    imagePath: 'assets/images/artista5.jpg',
+    category: '87,159,357',
+    description:
+        'Con sus acrobacias vocales de cuatro octavas y su imagen icónica, la cantante, compositora y actriz estadounidense Ariana Grande ha solidificado su lugar como una de las estrellas pop por excelencia de su generación, acumulando montones de premios y récords en el proceso.',
+    price: 4,
+    isFavorit: false,
+  ),
+  Cantantes(
+    id: 1,
+    name: 'The Weeknd',
+    imagePath: 'assets/images/artista6.jpg',
+    category: '111,546,321',
+    description:
+        'The Weeknd se hizo cargo de la música y la cultura pop en sus propios términos, filtrando el R&B, el pop y el hip-hop a través de una ambiciosa lente de pantalla ancha. El ganador del premio 3X GRAMMY multiplatino se ha convertido en uno de los artistas más exitosos e importantes de la era moderna.',
+    price: 2,
+    isFavorit: false,
+  ),
+  Cantantes(
+    id: 2,
+    name: 'Taylor Swift',
+    imagePath: 'assets/images/artista7.jpg',
+    category: '113,328,746',
+    description:
+        'En el amor y la poesía todo se vale… Nuevo álbum THE TORTURED POETS DEPARTMENT. Ya disponible 🤍',
+    price: 1,
+    isFavorit: false,
+  ),
+];
